928f8d665102bc6d3b0b70f2243cdbc5e7f9c9b5
devplutus <ji584738@gmail.com>
2021-05-04 17:04:54 +0900

    2020. 05. 04. - 20923 해결 중

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -29,60 +28,71 @@ func readIntegerAsArray() []int {
 	return cTemp
 }
 
-func indexOf(arr []int, find int) int {
-	for i, v := range arr {
-		if v == find {
-			return i
-		}
+func reverse(arr []int) []int {
+	temp := []int{}
+	for i := len(arr) - 1; i >= 0; i-- {
+		temp = append(temp, arr[i])
 	}
-	return -1
+	return temp
 }
 
 func main() {
 	defer writer.Flush()
+
 	parameters := readIntegerAsArray()
 	N, M := parameters[0], parameters[1]
-	words := map[string]int{}
+	// 0 Deck, 1 Ground
+	do := [2][]int{}
+	su := [2][]int{}
+	max := N
+	if N > M {
+		max = M
+	}
 
-	for i := 0; i < N; i++ {
-		word := readStringAsArray()[0]
-		if len(word) >= M {
-			if _, exist := words[word]; exist {
-				words[word] += 1
-			} else {
-				words[word] = 1
-			}
+	for i := 0; i < max; i++ {
+		parameters := readStringAsArray()
+		doN, _ := strconv.Atoi(parameters[0])
+		suN, _ := strconv.Atoi(parameters[1])
+		do[0] = append(do[0], doN)
+		su[0] = append(su[0], suN)
+	}
+
+	do[0] = reverse(do[0])
+	su[0] = reverse(su[0])
+
+	for i := 0; i < M; i++ {
+		// Flip
+		do[1] = append(do[1], do[0][len(do[0])-1])
+		su[1] = append(su[1], su[0][len(su[0])-1])
+		do[0] = do[0][:len(do[0])-1]
+		su[0] = su[0][:len(su[0])-1]
+		// su win
+		if do[1][len(do[1])-1] == 5 || su[1][len(su[1])-1] == 5 {
+			t := reverse(do[1])
+			su[0] = append(t, su[0]...)
+			t2 := reverse(su[1])
+			su[0] = append(t2, su[0]...)
+			su[1] = []int{}
+			do[1] = []int{}
+		} else if (len(do[1]) > 0 && len(su[1]) > 0) && do[1][len(do[1])-1]+su[1][len(su[1])-1] == 5 {
+			t := reverse(su[1])
+			do[0] = append(t, do[0]...)
+			t2 := reverse(do[1])
+			do[0] = append(t2, do[0]...)
+			su[1] = []int{}
+			do[1] = []int{}
+		}
+
+		if len(do[0]) == 0 || len(su[0]) == 0 {
+			break
 		}
 	}
 
-	type Word struct {
-		word  string
-		count int
+	if len(do[0]) == len(su[0]) {
+		fmt.Fprintln(writer, "dosu")
+	} else if len(do[0]) > len(su[0]) || (len(do[0]) > 0 && len(su[0]) == 0) {
+		fmt.Fprintln(writer, "do")
+	} else {
+		fmt.Fprintln(writer, "su")
 	}
-
-	arr := []Word{}
-
-	for k, v := range words {
-		arr = append(arr, Word{
-			word:  k,
-			count: v,
-		})
-	}
-
-	sort.Slice(arr, func(i, j int) bool {
-		if arr[i].count == arr[j].count {
-			if len(arr[i].word) == len(arr[j].word) {
-				return arr[i].word < arr[j].word
-			} else {
-				return len(arr[i].word) > len(arr[j].word)
-			}
-		} else {
-			return arr[i].count > arr[j].count
-		}
-	})
-
-	for _, v := range arr {
-		fmt.Fprintln(writer, v.word)
-	}
-
 }
