15c926c017e284e262990e99e1300f740d0a3eb4
devplutus <ji584738@gmail.com>
2021-05-21 08:59:13 +0900

    2020. 05. 21. - 파일 정리

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
-package main
-
 import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -28,59 +27,7 @@ func readIntegerAsArray() []int {
 	return cTemp
 }
 
-func cal(x, y, operator string) int {
-	cx, _ := strconv.Atoi(x)
-	cy, _ := strconv.Atoi(y)
-
-	if operator == "S" {
-		return cx - cy
-	} else if operator == "M" {
-		return cx * cy
-	} else if operator == "U" {
-		return cx / cy
-	} else {
-		return cx + cy
-	}
-}
-
 func main() {
 	defer writer.Flush()
 
-	readIntegerAsArray()
-	str := readStringAsArray()[0]
-
-	answer := []string{}
-	nums := [2]string{"", ""}
-	operator := []string{}
-
-	for _, s := range str {
-		if s >= '0' && s <= '9' {
-			if len(operator) == 0 {
-				nums[0] += string(s)
-			} else {
-				nums[1] += string(s)
-			}
-		} else {
-			if nums[1] != "" && len(operator) != 0 {
-				if operator[len(operator)-1] != "C" {
-					nums[0] = strconv.Itoa(cal(nums[0], nums[1], operator[len(operator)-1]))
-					nums[1] = ""
-				}
-				if string(s) == "C" {
-					temp, _ := strconv.Atoi(nums[0])
-					answer = append(answer, strconv.Itoa(temp))
-				}
-			} else if string(s) == "C" {
-				temp, _ := strconv.Atoi(nums[0])
-				answer = append(answer, strconv.Itoa(temp))
-			}
-			operator = append(operator, string(s))
-		}
-	}
-
-	if len(answer) == 0 {
-		fmt.Fprintln(writer, "NO OUTPUT")
-	} else {
-		fmt.Fprintln(writer, strings.Join(answer, " "))
-	}
 }
