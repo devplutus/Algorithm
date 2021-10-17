e8b2f0e5f40fc43b404555d50973ff265428882b
devplutus <ji584738@gmail.com>
2021-10-18 00:26:23 +0900

    2021. 10. 18. - LeetCode 83번, LinkedList

---
+++ LeetCode/83. Remove Duplicates from Sorted List.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var deleteDuplicates = function(head) {
+  if (head)  {
+      let current = head
+  
+      while(true) {
+          if (!current || !current.next) {
+              break
+          }
+          if (current.val === current.next.val) {
+              current.next = current.next.next
+          } else {
+              current = current.next
+          }
+      }
+      
+  }
+  
+  return head
+};
